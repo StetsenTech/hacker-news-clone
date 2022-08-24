1d952dad66f61dd87b33738f263e8c70737afb67
Brian Rose <stetsentech@gmail.com>
2022-08-24 16:53:32 -0400

    Adds item functionality

---
+++ components/Story.js
@@ -0,0 +1,22 @@
+export default function Story(story) {
+  console.log(story); 
+  return `
+    <div class="story">
+      <span class="gray">${story.index || ""}</span>
+      <span class="upvote">▲</span>
+      <a href="${story.url}">${story.title}</a>
+      <span>(${story.domain})</span>
+    </div>
+    <div>
+      <div class="gray">
+        ${story.points} points by ${story.user} ${story.time_ago}
+        <a href="#/item?id=${story.id}">
+          ${story.comments_count} comments
+        </a>
+        <span class="favorite">
+          <img class="heart" src="https://www.svgrepo.com/show/30066/heart.svg">
+        </span>
+      </div>
+    </div>
+  `;
+}
---
+++ pages/item.js
@@ -0,0 +1,35 @@
+import view from '../utils/view.js';
+
+export default async function Item() {
+  let story = null;
+  let hasComments = false;
+  let hasError = false;
+
+  try {
+    const story = await getStory();
+    const hasComments = story.comments.length > 6;
+  } catch(error) {
+    hasError = true;
+    console.error(error);
+  }
+
+  if (hasError) {
+    view.innerHTML = `<div class="error">Error fetching story</div>`;
+  }
+ 
+  view.innerHTML = `
+    <div>
+      ${Story(story)}
+    </div>
+    <hr/>
+    ${hasComments ? story.comments.map(comment => JSON.stringify(comment)).join('') : "No Comment"}
+  `;
+}
+
+async function getStory() {
+  const storyId = window.location.hash.split('?id=');
+  const response = await fetch(`https://node-hnapi.herokuapp.com/items/${storyId}`);
+  const story = await response.json();
+
+  return story;
+}
